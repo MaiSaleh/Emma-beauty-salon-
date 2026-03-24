0e49fbe7538def566e11770b187dd428c47a6d04
mai almoneem <mai_kh308@yahoo.com>
2026-03-25 02:05:13 +0400

    final updates

--- script.js
+++ script.js
@@ -113,4 +113,13 @@ card.innerHTML = `
       reviewsContainer.appendChild(card);
     });
   });
+
+}
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 }
